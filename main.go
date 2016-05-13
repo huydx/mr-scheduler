bab34a52db7f875d68044b8e374091db4597bcf4
dxhuy1988 <doxuanhuy@gmail.com>
2016-05-13 22:26:16 +0900

    refactor view as partial view

--- main.go
+++ main.go
@@ -11,10 +11,6 @@ func main() {
 	router.LoadHTMLGlob("./static/html/*")
 
 	router.GET("/index", LandingHandler)
-	router.GET("/memo", LandingHandler)
-	router.GET("/dateselect", LandingHandler)
-	router.GET("/final", LandingHandler)
-
 	router.Run(":8080")
 }
 
@@ -24,25 +20,7 @@ func LandingHandler(g *gin.Context) {
 	d := NewDbHandler("localhost")
 	e := &(Event{"2222", "fooo"})
 	d.SetEvent(e)
-	g.HTML(http.StatusOK, "landing.tmpl", gin.H{
+	g.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"hash": "213132313",
 	})
-}
-
-func MemoGetHandler(g *gin.Context) {
-	//get hash from static by form POST
-	//render memo
-	g.HTML(http.StatusOK, "memo.tmpl", gin.H{})
-}
-
-func DateSelectHandler(g *gin.Context) {
-	//get hash + info from memo by form POST
-	//render date select
-	g.HTML(http.StatusOK, "selectdate.tmpl", gin.H{})
-}
-
-func FinishHandler(g *gin.Context) {
-	//get hash from date select by form POST
-	//render link with hash
-	g.HTML(http.StatusOK, "final.tmpl", gin.H{})
 }
